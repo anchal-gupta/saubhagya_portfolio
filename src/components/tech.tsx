00e50c8d6cb093c7e9e56cd7c0ed1f281ecfa149
Anchal <anchalchery@gmail.com>
2025-02-10 18:42:55 +0530

    Fix: Text and logo

--- src/components/tech.tsx
+++ src/components/tech.tsx
@@ -9,8 +9,18 @@ export const Tech = () => {
       <div className="flex flex-row flex-wrap justify-center gap-10">
         {/* Iterate over each technology */}
         {TECHNOLOGIES.map((technology) => (
-          <div className="w-28 h-28" key={technology.name}>
+          <div 
+            className="w-28 h-28 relative group" 
+            key={technology.name}
+          >
             <BallCanvas icon={technology.icon} />
+            {/* Tooltip */}
+            <div className="absolute -bottom-10 left-1/2 transform -translate-x-1/2 
+                          bg-black bg-opacity-75 text-white px-2 py-1 rounded-md 
+                          text-sm opacity-0 group-hover:opacity-100 transition-opacity 
+                          duration-300 whitespace-nowrap">
+              {technology.name}
+            </div>
           </div>
         ))}
       </div>
